3a8b50a08e03ec85e0de96c443a2166efab9b211
ACMANI-LOVE <atomos.fee24@gmail.com>
2025-01-19 01:27:15 +0900

    joining frontend

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -6,31 +6,32 @@ interface APIRequest {
 // "configs": `http://localhost:8000/configs/`,
 export const getConfigsAPI    = () => { fetcher("GET", `http://localhost:8000/configs/`)}
 export const postConfigsAPI   = ({request}:APIRequest) => { fetcher("POST", `http://localhost:8000/configs/`, request)}
-export const putConfigsAPI    = ({request}:APIRequest) => { fetcher("PUT" , `http://localhost:8000/configs/`, request)}
+export const putConfigsAPI    = ({prmText,request}:APIRequest) => { fetcher("PUT" , `http://localhost:8000/configs/${prmText}/`, request)}
 
 // "groups": `http://localhost:8000/groups/`,
 export const getGroupsAPI    = () => { fetcher("GET" , `http://localhost:8000/groups/`)}
 export const postGroupsAPI   = ({request}:APIRequest) => { fetcher("POST", `http://localhost:8000/groups/`, request)}
-export const patchGroupsAPI  = ({prmText,request}:APIRequest) => { fetcher("PATCH" , `http://localhost:8000/groups/${prmText}`, request)}
-export const deleteGroupsAPI = ({prmText,request}:APIRequest) => { fetcher("DELETE", `http://localhost:8000/groups/${prmText}`, request)}
+export const patchGroupsAPI  = ({prmText,request}:APIRequest) => { fetcher("PATCH" , `http://localhost:8000/groups/${prmText}/`, request)}
+export const deleteGroupsAPI = ({prmText,request}:APIRequest) => { fetcher("DELETE", `http://localhost:8000/groups/${prmText}/`)}
 
 // "tasks": `http://localhost:8000/tasks/`,
-export const getTasksAPI    = ({prmText}:APIRequest) => { fetcher("GET", `http://localhost:8000/tasks/${prmText}`)}
+export const getTasksAPI    = ({prmText}:APIRequest) => { fetcher("GET", `http://localhost:8000/tasks/${prmText}/`)}
 
 // "posts": `http://localhost:8000/posts/`,
 export const postPostsAPI   = ({request}:APIRequest) => { fetcher("POST" , `http://localhost:8000/posts/`     , request)}
-export const patchPostsAPI  = ({prmText,request}:APIRequest) => { fetcher("PATCH", `http://localhost:8000/posts/${prmText}`, request)}
+export const patchPostsAPI  = ({prmText,request}:APIRequest) => { fetcher("PATCH", `http://localhost:8000/posts/${prmText}/`, request)}
 
 // "prompts": `http://localhost:8000/prompts/`,
 export const postPromptsAPI   = ({request}:APIRequest) => { fetcher("POST" , `http://localhost:8000/prompts/`     , request)}
-export const patchPromptsAPI  = ({prmText,request}:APIRequest) => { fetcher("PATCH", `http://localhost:8000/prompts/${prmText}`, request)}
+export const patchPromptsAPI  = ({prmText,request}:APIRequest) => { fetcher("PATCH", `http://localhost:8000/prompts/${prmText}/`, request)}
 
 // "requests": `http://localhost:8000/requests/`,
 export const postRequestsAPI   = ({request}:APIRequest) => { fetcher("POST" , `http://localhost:8000/requests/`     , request)}
-export const patchRequestsAPI  = ({prmText,request}:APIRequest) => { fetcher("PATCH", `http://localhost:8000/requests/${prmText}`, request)}
+export const patchRequestsAPI  = ({prmText,request}:APIRequest) => { fetcher("PATCH", `http://localhost:8000/requests/${prmText}/`, request)}
 
 
 const fetcher = async (method:string, url:string, request?:object) => {
+  console.log(JSON.stringify(request))
   try {
     const response = await fetch(url, {
       method,
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,37 +1,43 @@
 'use client'
-import { Button } from "@mui/material";
+import { Box, Button, Container } from "@mui/material";
 import { deleteGroupsAPI, getConfigsAPI, getGroupsAPI, getTasksAPI, patchGroupsAPI, patchPostsAPI, patchPromptsAPI, patchRequestsAPI, postConfigsAPI, postGroupsAPI, postPostsAPI, postPromptsAPI, postRequestsAPI, putConfigsAPI } from "@/api/api"
 
 const Home = () => {
   const getConfigs    = () => getConfigsAPI   ()
-  const putConfigs    = () => putConfigsAPI   ({request:{}})
-  const postConfigs   = () => postConfigsAPI  ({request:{}})
+  const putConfigs    = () => putConfigsAPI   ({prmText:"1", request:{test:0}})
+  const postConfigs   = () => postConfigsAPI  ({request:{isLewd:true}})
   const getGroups     = () => getGroupsAPI    ()
-  const postGroups    = () => postGroupsAPI   ({request:{}})
-  const patchGroups   = () => patchGroupsAPI  ({prmText:"", request:{}})
-  const deleteGroups  = () => deleteGroupsAPI ({prmText:"", request:{}})
-  const getTasks      = () => getTasksAPI     ({prmText:""})
+  const postGroups    = () => postGroupsAPI   ({request:{test:0}})
+  const patchGroups   = () => patchGroupsAPI  ({prmText:"1", request:{test:0}})
+  const deleteGroups  = () => deleteGroupsAPI ({prmText:"1"})
+  const getTasks      = () => getTasksAPI     ({prmText:"1"})
   const postPosts     = () => postPostsAPI    ({request:{}})
-  const patchPosts    = () => patchPostsAPI   ({prmText:"", request:{}})
+  const patchPosts    = () => patchPostsAPI   ({prmText:"1", request:{test:0}})
   const postPrompts   = () => postPromptsAPI  ({request:{}})
-  const patchPrompts  = () => patchPromptsAPI ({prmText:"", request:{}})
+  const patchPrompts  = () => patchPromptsAPI ({prmText:"1", request:{test:0}})
   const postRequests  = () => postRequestsAPI ({request:{}})
-  const patchRequests = () => patchRequestsAPI({prmText:"", request:{}})
-  return (<div>
+  const patchRequests = () => patchRequestsAPI({prmText:"1", request:{test:0}})
+  return (<Box display={"flex"} flexDirection={"column"}>
+    Config
     <Button variant="contained" onClick={getConfigs    }>getConfigsAPI   </Button>
     <Button variant="contained" onClick={putConfigs    }>putConfigsAPI   </Button>
     <Button variant="contained" onClick={postConfigs   }>postConfigsAPI  </Button>
+    Groups
     <Button variant="contained" onClick={getGroups     }>getGroupAPI     </Button>
     <Button variant="contained" onClick={postGroups    }>postGroupAPI    </Button>
     <Button variant="contained" onClick={patchGroups   }>patchGroupAPI   </Button>
     <Button variant="contained" onClick={deleteGroups  }>deleteGroupAPI  </Button>
+    Tasks
     <Button variant="contained" onClick={getTasks      }>getTaskAPI      </Button>
+    Posts
     <Button variant="contained" onClick={postPosts     }>postPostsAPI    </Button>
     <Button variant="contained" onClick={patchPosts    }>patchPostsAPI   </Button>
+    Prompts
     <Button variant="contained" onClick={postPrompts   }>postPromptsAPI  </Button>
     <Button variant="contained" onClick={patchPrompts  }>patchPromptsAPI </Button>
+    Requests
     <Button variant="contained" onClick={postRequests  }>postRequestsAPI </Button>
     <Button variant="contained" onClick={patchRequests }>patchRequestsAPI</Button>
-  </div>);
+  </Box>);
 }
 export default Home
